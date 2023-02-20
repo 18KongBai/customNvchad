44f539b09b502abea56897a1882d578f2bf9ea2b
kanghuhui <869576471@qq.com>
2023-02-20 14:27:32 +0800

    feat: 开启透明度配置

--- chadrc.lua
+++ chadrc.lua
@@ -6,7 +6,8 @@ local highlights = require "custom.highlights"
 
 M.ui = {
   theme_toggle = { "everforest", "everforest_light" },
-  theme = "kanagawa",
+  transparency = true,
+  theme = "everforest",
   changed_themes = themes,
   hl_override = highlights.override,
   hl_add = highlights.add,
