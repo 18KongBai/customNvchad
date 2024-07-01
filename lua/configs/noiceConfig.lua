20567bdfa79ee7a63e59ee4ae267b18a1d1350c0
kanghuhui <869576471@qq.com>
2024-07-01 19:32:19 +0800

    fix: 报错

--- lua/configs/noiceConfig.lua
+++ lua/configs/noiceConfig.lua
@@ -6,6 +6,12 @@ local options = {
       ["vim.lsp.util.stylize_markdown"] = true,
       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
     },
+    signature = {
+      enabled = false,
+    },
+    hover = {
+      enabled = false,
+    },
   },
   -- you can enable a preset for easier configuration
   presets = {
